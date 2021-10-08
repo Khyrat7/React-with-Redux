73cb2f4f5eb5c1d91dbccdc8029e67cbea90b0cb
@khyrat7 <ahmedkhyrat@gmail.com>
2021-10-08 15:09:09 +0200

    Async/Await

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -24,14 +24,16 @@ class CoursesPage extends React.Component {
   }
 
   //
-  handleDeleteCourse = (course) => {
+  handleDeleteCourse = async (course) => {
     toast.success("Course deleted");
     // there is and error to fix ( no error is exproted )
-    this.props.actions.deleteCourse(course).catch((error) => {
+    try {
+      await this.props.actions.deleteCourse(course);
+    } catch (error) {
       toast.error("Delete failed. " + error.message, {
         autoClose: false,
       });
-    });
+    }
   };
 
   render() {
